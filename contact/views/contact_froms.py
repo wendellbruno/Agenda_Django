72508bb1b14e63ca216b38c6821a8304152e229e
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2023-05-10 11:11:09 -0300

    commit 06
    commit 06

--- contact/views/contact_froms.py
+++ contact/views/contact_froms.py
@@ -3,14 +3,50 @@ from django.db.models import Q
 from contact.models import Contact
 from django.core.paginator import Paginator
 from django.http import Http404
+from django import forms
+from django.core.exceptions import ValidationError
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = (
+            'first_name',
+            'last_name',
+            'phone',
+            )
+    
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        self.add_error(
+            'first_name',
+            ValidationError(
+            'Mensagem de erro',
+            code = 'invalid'
+            )
+        )
+        return super().clean()
 
 def create(request):
-    context = {
+
+    if request.method == 'POST':
+
+        context = {
+        'forms': ContactForm(data=request.POST)
+        }
         
+        return render(
+            request,
+            'contact/create.html',
+            context = context
+        )
+
+
+    context = {
+        'forms': ContactForm()
     }
 
     return render(
-        request,
-        'contact/create.html',
-        context = context
-    )
+            request,
+            'contact/create.html',
+            context = context
+        )
